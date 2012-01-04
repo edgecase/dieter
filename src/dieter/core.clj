f10fe8c2ad59b708f4842bb4f5de365e3faac3c1
John Andrews + Andrew Grieser <pairing+ja+ag@edgecase.com>
2012-01-04 09:42:05 -0500

    FIRST

---
+++ src/dieter/core.clj
@@ -0,0 +1,1 @@
+(ns dieter.core)
---
+++ test/dieter/test/core.clj
@@ -0,0 +1,6 @@
+(ns dieter.test.core
+  (:use [dieter.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

356560890691fd4d9dd414e89585249933c0ecd4
John Andrews + Andrew Grieser <pairing+ja+ag@edgecase.com>
2012-01-10 15:59:00 -0500

    Handlebars

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "(inc sprockets)"
   :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :dependencies [[ring "1.0.1"]
-                 ;[yuicompressor "2.4.7"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [rhino/js "1.7R2"]
                  [org.clojure/clojure "1.3.0"]])
--- src/dieter/compressor.clj
+++ src/dieter/compressor.clj
@@ -1,31 +1,7 @@
 (ns dieter.compressor
-  (:require [clojure.java.io :as io])
-  (:import [java.io StringReader StringWriter]
-           ;[com.yahoo.platform.yui.compressor JavaScriptCompressor CssCompressor]
-           [com.google.javascript.jscomp JSSourceFile CompilerOptions CompilationLevel CommandLineRunner]
-           [org.mozilla.javascript ErrorReporter]))
-
-(def reporter (reify ErrorReporter
-                (warning [this message source-name line line-source line-offset] (println message))
-                (error [this message source-name line line-source line-offset] (println message))
-                (runtimeError [this message source-name line line-source line-offset] (Exception. message))))
-
-; can this come in as a string, or always a file? Where does the
-; decision to compress as js or css made?
-;(defn compress [input-file output-file level])
-
-;; (defn compress-yui-js [text]
-;;   (let [input (StringReader. text)
-;;         output (StringWriter.)
-;;         compressor (JavaScriptCompressor. input reporter)]
-;;     (.compress compressor output
-;;                -1    ; line break
-;;                true  ; munge (shorten var names)
-;;                false ; verbose
-;;                false ; preserve all semicolons
-;;                false ; disable optimizations
-;;                )
-;;     (str output)))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as cstr])
+  (:import [com.google.javascript.jscomp JSSourceFile CompilerOptions CompilationLevel]))
 
 (defn compress-js [text]
   (let [compiler (com.google.javascript.jscomp.Compiler.)
@@ -34,16 +10,7 @@
     (.compile compiler (make-array JSSourceFile 0)
               (into-array JSSourceFile [(JSSourceFile/fromCode "awesome-js" text)])
               options)
-    (.toSource compiler)
-    )
-  )
+    (.toSource compiler)))
 
 (defn compress-css [text]
-  text
-
-;;   (with-open [input (StringReader. text)
-;;               output (StringWriter.)]
-;;     (let [compressor (CssCompressor. input)]
-;;       (.compress compressor output -1)
-  ;;       (str output)))
-)
+  (cstr/replace (cstr/replace text "\n" "") #"\s+" " "))

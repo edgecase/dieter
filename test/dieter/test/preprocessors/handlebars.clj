4da2f7d25c44c4c8733fd54dac5f4478b1d6aaa2
John Andrews <john@edgecase.com>
2012-01-28 16:00:48 -0500

    add poor man's perf test

--- test/dieter/test/preprocessors/handlebars.clj
+++ test/dieter/test/preprocessors/handlebars.clj
@@ -8,9 +8,9 @@
 (deftest test-preprocess-handlebars
   (testing "ember mode"
     (binding [*settings* (merge *settings* {:hbs-mode :ember})]
-      (is (has-text? (preprocess-handlebars (io/file "test/fixtures/assets/javascripts/view.hbs"))
-                     "Ember.TEMPLATES[\"view\"]=Ember.Handlebars.template"))))
+      (dotimes [n 5] (is (has-text? (preprocess-handlebars (io/file "test/fixtures/assets/javascripts/view.hbs"))
+                                    "Ember.TEMPLATES[\"view\"]=Ember.Handlebars.template")))))
   (testing "handlebars mode"
     (binding [*settings* (merge *settings* {:hbs-mode :handlebars})]
-      (is (has-text? (preprocess-handlebars (io/file "test/fixtures/assets/javascripts/view.hbs"))
-                     "Handlebars.templates[\"view\"]=Handlebars.template")))))
+      (dotimes [n 5] (is (has-text? (preprocess-handlebars (io/file "test/fixtures/assets/javascripts/view.hbs"))
+                                    "Handlebars.templates[\"view\"]=Handlebars.template"))))))

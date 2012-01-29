3bc8e4a87b8ceeb95a236fe6e5afcf30aec734b4
John Andrews <john@edgecase.com>
2012-01-29 14:55:36 -0500

    improve rhino performance by reusing scopes

--- src/dieter/preprocessors/handlebars.clj
+++ src/dieter/preprocessors/handlebars.clj
@@ -1,29 +1,24 @@
 (ns dieter.preprocessors.handlebars
   (:require [clojure.java.io :as io]
             [clojure.string :as cstr])
-  (:use dieter.preprocessors.rhino
+  (:use [dieter.preprocessors.rhino :only [defscope call]]
         [dieter.settings :only [*settings*]])
   (:import [org.mozilla.javascript Context NativeObject]))
 
 (defn filename-without-ext [file]
   (cstr/replace (.getName file) #"\..*$" ""))
 
-(def context (Context/enter))
-(def scope (.initStandardObjects context))
-(load-vendor "hbs-wrapper.js" context scope)
-(load-vendor "ember-0.9.4.js" context scope)
-(def ember-fn (.get scope "precompileEmber" scope))
-(def handlebars-fn (.get scope "precompileHandlebars" scope))
+(defscope scope "hbs-wrapper.js" "ember-0.9.4.js")
 
 (defn compile-ember [string filename]
   (str "Ember.TEMPLATES[\"" filename "\"]=Ember.Handlebars.template("
-       (.call ember-fn context scope nil (into-array [string]))
+       (call "precompileEmber" scope string)
        ");"))
 
 (defn compile-handlebars [string filename]
   (str "Handlebars.templates = Handlebars.templates || {};"
        "Handlebars.templates[\"" filename "\"]=Handlebars.template("
-       (.call handlebars-fn context scope nil (into-array [string]))
+       (call "precompileHandlebars" scope string)
        ");"))
 
 (defn preprocess-handlebars [file]
--- src/dieter/preprocessors/rhino.clj
+++ src/dieter/preprocessors/rhino.clj
@@ -2,11 +2,22 @@
   (:require [clojure.java.io :as io])
   (:import [org.mozilla.javascript Context NativeObject]))
 
-(defmacro with-rhino [context scope & body]
-  `(let [~context (Context/enter)
-         ~scope (.initStandardObjects ~context)]
+(defmacro with-context [context & body]
+  `(let [~context (Context/enter)]
      (try ~@body
           (finally (Context/exit)))))
+
+(defmacro defscope [name & preloads]
+  `(with-context context#
+     (let [scope#  (.initStandardObjects context#)]
+       (doseq [file# '~preloads]
+         (load-vendor file# context# scope#))
+       (defonce ~name scope#))))
+
+(defn call [fn-name scope & args]
+  (with-context cx
+    (let [fun (.get scope fn-name scope)]
+      (.call fun cx scope nil (into-array args)))))
 
 (defn load-vendor [filename context scope]
   (.evaluateReader context scope

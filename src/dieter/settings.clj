4596f0f8791d2fb70a599d47dd7fa84919b91fbe
Paul Biggar <paul.biggar@gmail.com>
2012-04-16 00:49:05 -0700

    Add utility functions to add to cached-paths and to bind *settings*

--- src/dieter/settings.clj
+++ src/dieter/settings.clj
@@ -9,7 +9,14 @@
    :hbs-mode   :handlebars
    :log-level  :normal})
 
+(defmacro with-options [options & body]
+  `(binding [*settings* (merge *settings* ~options)]
+     (do ~@body)))
+
 (defonce cached-paths (atom {}))
+
+(defn add-cached-path [path new-path]
+  (swap! cached-paths assoc path new-path))
 
 (defn asset-root []
   (:asset-root *settings*))

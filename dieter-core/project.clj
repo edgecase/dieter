13b1961108bb290a5f1b904b9a563c3c728d435c
Paul Biggar <paul.biggar@gmail.com>
2012-05-13 10:25:27 -0700

    Add v8 dependencies

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -4,5 +4,8 @@
                  [ring "1.0.1"]
                  [fs "0.11.1"]
                  [com.google.javascript/closure-compiler "r1592"]
+                 [clj-v8 "0.1.0"]
                  [rhino/js "1.7R2"]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
+  :native-dependencies [[clj-v8-native "0.1.0"]]
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]
+                     [redd/native-deps "1.0.7"]])

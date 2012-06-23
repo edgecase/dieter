117454af4b905c4570cc7f8c939be7c28fbbe343
Ivan Stojic <ivans@ordecon.com>
2012-06-23 08:45:12 +0200

    Basic JS engine separation for CoffeeScript.
    Implemented jsengine.clj as a layer separating things that need to
    execute JS from the actual engine implementation. Created stub code for
    the V8 that mimics Rhino by signature, but is a no-op for now. Rewritten
    cofeescript.clj asset compiler in terms of the new jsengine.clj instead
    of binding directly to Rhino.

---
+++ dieter-core/src/dieter/jsengine.clj
@@ -0,0 +1,21 @@
+(ns dieter.jsengine
+  (:require [dieter.settings :as settings]
+            [dieter.pools :as pools]
+            [dieter.rhino :as rhino]
+            [dieter.v8 :as v8]))
+
+
+(defmacro with-context [& body]
+  (if (= (:engine settings/*settings*) :rhino)
+    `(rhino/with-context ~@body)
+    `(v8/with-context ~@body)))
+
+(defmacro with-scope [pool preloads & body]
+  (if (= (:engine settings/*settings*) :rhino)
+    `(rhino/with-scope ~pool ~preloads ~@body)
+    `(v8/with-scope ~pool ~preloads ~@body)))
+
+(defn call [fn-name & args]
+  (if (= (:engine settings/*settings*) :rhino)
+    (apply rhino/call fn-name args)
+    (v8/call fn-name args)))
---
+++ dieter-core/src/dieter/v8.clj
@@ -0,0 +1,11 @@
+(ns dieter.v8)
+
+(defmacro with-context [& body]
+  (comment nothing yet))
+
+(defmacro with-scope [pool preloads & body]
+  (comment nothing yet))
+
+(defn call [fn-name & args]
+  (comment nothing yet))
+

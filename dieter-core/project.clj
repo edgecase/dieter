f62e67775ba815b3d8e2b26d38ffccf4db473819
Ivan Stojic <ivans@ordecon.com>
2012-06-24 16:12:07 +0200

    reorganize core project descriptor so that v8 actually works

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -5,7 +5,10 @@
                  [fs "0.11.1"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [clj-v8 "0.1.0"]
+                 [clj-v8-native "0.1.1"]
                  [rhino/js "1.7R2"]]
-  :native-dependencies [[clj-v8-native "0.1.0"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
-                     [redd/native-deps "1.0.7"]])
+                     [redd/native-deps "1.0.7"]]
+  :jvm-opts ["-Djna.library.path=native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
+
+

9da9488b02db8dd5b69f01896f541e8e2b512674
Ivan Stojic <ivans@ordecon.com>
2012-06-27 20:44:11 +0200

    test project for the issue #10 - adding expire headers

---
+++ test-projects/t3/project.clj
@@ -0,0 +1,7 @@
+(defproject t3 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [ring/ring-core "1.1.1"]
+                 [ring/ring-jetty-adapter "1.1.1"]
+                 [org.eclipse.jetty/jetty-server "7.6.1.v20120215"]
+                 [dieter/dieter "0.2.2"]])
---
+++ test-projects/t3/test/t3/test/core.clj
@@ -0,0 +1,6 @@
+(ns t3.test.core
+  (:use [t3.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

f779538655a467fa21772a636d40eca441a82678
Paul Biggar <paul.biggar@gmail.com>
2012-07-11 14:19:20 -0700

    Use updated clj-v8, with working v8

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -4,11 +4,9 @@
                  [ring "1.0.1"]
                  [fs "0.11.1"]
                  [com.google.javascript/closure-compiler "r1592"]
-                 [clj-v8 "0.1.1"]
-                 [clj-v8-native "0.1.1"]
+                 [clj-v8 "0.1.2"]
+                 [clj-v8-native "0.1.2"]
                  [rhino/js "1.7R2"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
                      [redd/native-deps "1.0.7"]]
   :jvm-opts ["-Djna.library.path=native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
-
-

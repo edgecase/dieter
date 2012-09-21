a5b262d831d7d04941f5bc92b60e2d69dd698807
Tim Harper <timcharper@gmail.com>
2012-09-21 14:10:44 -0600

    Make less compile errors visible in page via css content property

--- dieter-core/src/dieter/asset/less.clj
+++ dieter-core/src/dieter/asset/less.clj
@@ -5,13 +5,27 @@
    dieter.asset.css
    [dieter.pools :as pools]
    [clojure.string :as cstr]
-   [dieter.asset :as asset]))
+   [dieter.asset :as asset]
+   [clojure.string :as string]))
 
 (def pool (pools/make-pool))
 
+(defn sanitize-css-content [str]
+  (string/replace
+   (string/replace
+    str
+    "\"" "\\\"" )
+   "\n" "\\A"))
+
 (defn preprocess-less [file]
-  (with-scope pool ["less-wrapper.js" "less-rhino-1.3.0.js"]
-    (call "compileLess" (.getCanonicalPath file))))
+  (let [filename (.getCanonicalPath file)]
+    (try
+      (with-scope pool ["less-wrapper.js" "less-rhino-1.3.0.js"]
+        (call "compileLess" filename))
+      (catch org.mozilla.javascript.JavaScriptException e
+        (format "body:before { content: \"In file %s - %s\"; white-space: pre }"
+                filename
+                (sanitize-css-content (.getMessage e)))))))
 
 (defrecord Less [file]
   dieter.asset.Asset

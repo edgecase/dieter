cf7e959c6c63fc5165bd7f55ad5e133ce617c258
Paul Biggar <paul.biggar@gmail.com>
2012-11-19 14:29:15 -0800

    Update haml_coffee_assets from upstream

--- dieter-core/resources/vendor/haml_coffee_assets.js
+++ dieter-core/resources/vendor/haml_coffee_assets.js
@@ -1,18 +1,9 @@
 /**
- * Fix for the Ruby Rhino
- */
-if (!String.prototype.trim) {
-    String.prototype.trim = function () {
-        return this.replace(/^\s*/, "").replace(/\s*$/, "");
-    }
-}
-
-/**
  * Haml Coffee Asset compiler.
  */
 var HamlCoffeeAssets = (function(){
 
-    var Compiler = require('/haml-coffee.js');
+    var Compiler = require('./haml-coffee');
 
     return {
 
@@ -21,31 +12,32 @@ var HamlCoffeeAssets = (function(){
        *
        * @param name [String] the name of the template that is registered to the namespace
        * @param source [String] the template source code to be compiled
-       * @param jst [Boolean] if a JST template should be generated
        * @param namespace [String] the template namespace
+       * @param jst [Boolean] if a JST template should be generated
        * @param format [String] output HTML format
        * @param uglify [Boolean] skip HTML indention
        * @param basename [Boolean] ignore path when generate JST
        * @param escapeHtml [Boolean] whether to escape HTML output by default or not
        * @param escapeAttributes [Boolean] whether to escape HTML attributes output by default or not
-       * @param cleanValue [Boolean]
+       * @param cleanValue [String] the name of the function to clean the values
+       * @param placement [String] where to place the function, either `global` or `amd`
+       * @param dependencies [Object] the amd module dependencies
        * @param customHtmlEscape [String] the name of the function to escape the output
        * @param customCleanValue [String] the name of the function to clean the code output
-       * @param customPreserve [String] the name of the function for the preserve helper
-       * @param customFindAndPreserver [String] the name of the function for the findAndPreserver helper
        * @param customSurround [String] the name of the function for the surround helper
        * @param customSucceed [String] the name of the function for the succeed helper
        * @param customPrecede [String] the name of the function for the precede helper
        * @param preserveTags [String] comma separated list of tags to preserve whitespace
        * @param selfCloseTags [String] comma separated list of tags to self-closing tags
        * @param context [String] the name of the function to merge contexts
+       * @param extendScope [Boolean] extend the scope with the context
        */
       compile: function(name, source, jst, namespace, format, uglify, basename,
-                        escapeHtml, escapeAttributes, cleanValue,
+                        escapeHtml, escapeAttributes, cleanValue, placement, dependencies,
                         customHtmlEscape, customCleanValue, customPreserve, customFindAndPreserve,
                         customSurround, customSucceed, customPrecede,
                         preserveTags, selfCloseTags,
-                        context) {
+                        context, extendScope) {
 
         var compiler = new Compiler({
           format: format,
@@ -54,6 +46,8 @@ var HamlCoffeeAssets = (function(){
           escapeHtml: escapeHtml,
           escapeAttributes: escapeAttributes,
           cleanValue: cleanValue,
+          placement: placement,
+          dependencies: dependencies,
           customHtmlEscape: customHtmlEscape,
           customCleanValue: customCleanValue,
           customPreserve: customPreserve,
@@ -62,7 +56,8 @@ var HamlCoffeeAssets = (function(){
           customSucceed: customSucceed,
           customPrecede: customPrecede,
           preserveTags: preserveTags,
-          selfCloseTags: selfCloseTags
+          selfCloseTags: selfCloseTags,
+          extendScope: extendScope
         });
 
         compiler.parse(source);
@@ -71,13 +66,26 @@ var HamlCoffeeAssets = (function(){
 
         if (jst) {
           template = CoffeeScript.compile(compiler.render(name, namespace));
+
         } else {
           var hamlc = CoffeeScript.compile(compiler.precompile(), { bare: true });
-          template = '(function(context) {\n  return (function() {\n' + hamlc.replace(/^(.*)$/mg, '    $1') + '\n  }).call(context);\n});';
+
+          if (extendScope) {
+            template = '(function(context) {\n  with(context || {}) {\n    return (function() {\n' + hamlc.replace(/^(.*)\n$/mg, '      $1') + '\n    }).call(context);\n  };\n});\n';
+          } else {
+            template = '(function(context) {\n  return (function() {\n' + hamlc.replace(/^(.*)\n$/mg, '    $1') + '\n  }).call(context);\n});\n';
+          }
         }
 
-        if (context !== '') {
-          template = template.replace('.call(context);', '.call(' + context + '(context));');
+        if (context !== '' && context !== false) {
+          // With AMD we replace the context function with the one from the hamlcoffee AMD module if not set explicit 
+          if (placement === 'amd' && context === 'window.HAML.context' && /^hamlcoffee/.test(dependencies['hc'])) {
+              context = 'hc.context';
+          }
+          // Only add the global template context for global placement, or when amd with the hamlcoffe module context
+          if (placement === 'global' || (placement === 'amd' && context == 'hc.context')) {
+              template = template.replace('.call(context);', '.call(' + context + '(context));');
+          }
         }
 
         return template;

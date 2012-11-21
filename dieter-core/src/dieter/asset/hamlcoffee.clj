bc1bf8780f21d82e117b8b21a0a7558b0927a149
Paul Biggar <paul.biggar@gmail.com>
2012-11-20 18:45:54 -0800

    Switch to using run-compiler for hamlcoffee

--- dieter-core/src/dieter/asset/hamlcoffee.clj
+++ dieter-core/src/dieter/asset/hamlcoffee.clj
@@ -1,26 +1,24 @@
 (ns dieter.asset.hamlcoffee
   (:require [dieter.pools :as pools]
-            dieter.asset.javascript
-            dieter.asset
-            [clojure.string :as cstr])
-  (:use [dieter.rhino :only (with-scope call)]))
-
-(defn filename-without-ext [file]
-  (cstr/replace (.getName file) #"\..*$" ""))
+            [clojure.string :as cstr]
+            [dieter.asset]
+            [dieter.asset.javascript])
+  (:use [dieter.jsengine :only (run-compiler)]))
 
 (def pool (pools/make-pool))
 
 (defn preprocess-hamlcoffee [file]
-  (with-scope pool ["coffee-script.js"
-                    ;; imported direct from https://raw.github.com/netzpirat/haml-coffee/master/dist/compiler/hamlcoffee.js
-                    "hamlcoffee.js"
-                    "haml_coffee_assets-rhino-fix.js"
-                    ;; imported direct from https://raw.github.com/netzpirat/haml_coffee_assets/master/lib/js/haml_coffee_assets.js
-                    "haml_coffee_assets.js"
-                    "hamlcoffee-wrapper.js"]
-    (let [input (slurp file)
-          filename (filename-without-ext file)]
-      (call "compileHamlCoffee" input filename))))
+  (run-compiler pool
+                ["coffee-script.js"
+                 ;; imported direct from https://raw.github.com/netzpirat/haml-coffee/master/dist/compiler/hamlcoffee.js
+                 "hamlcoffee.js"
+                 "haml_coffee_assets-rhino-fix.js"
+                 ;; imported direct from https://raw.github.com/netzpirat/haml_coffee_assets/master/lib/js/haml_coffee_assets.js
+                 "haml_coffee_assets.js"
+                 "hamlcoffee-wrapper.js"]
+                "compileHamlCoffee"
+                file
+                :engine :rhino)) ; still crashes
 
 (defrecord HamlCoffee [file]
   dieter.asset.Asset

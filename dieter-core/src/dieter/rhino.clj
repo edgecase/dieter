bc8c339ff155d14e374cae1e8b6cb7f62c7a3406
Paul Biggar <paul.biggar@gmail.com>
2012-11-20 18:13:45 -0800

    Remove dead code

--- dieter-core/src/dieter/rhino.clj
+++ dieter-core/src/dieter/rhino.clj
@@ -15,7 +15,6 @@
       (load-vendor file scope))
     scope))
 
-
 (defmacro with-context [& body]
   `(binding [context (Context/enter)]
      (.setOptimizationLevel context -1) ; Rhino hits a 64K limit when compiling
@@ -36,19 +35,7 @@
   (let [#^org.mozilla.javascript.InterpretedFunction fun (.get scope fn-name scope)]
     (.call fun context scope nil (into-array args))))
 
-(defn getvar
-  ([name]
-     (getvar name scope))
-  ([name obj]
-     (.get scope name obj)))
-
-(defn setvar [name val]
-  (.put scope name scope val))
-
 (defn load-vendor [filename scope]
   (.evaluateReader context scope
                    (io/reader (io/resource (str "vendor/" filename)))
                    filename 1 nil))
-
-(defn js-keys [obj]
-  (seq (NativeObject/getPropertyIds obj)))

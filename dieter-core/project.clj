84b6154b81bdb8d2c6a85109cc92b6471e77e803
Paul Biggar <paul.biggar@gmail.com>
2012-11-20 21:18:07 -0800

    Use latest version of Rhino, which should have much better performance

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -6,7 +6,7 @@
                  [com.google.javascript/closure-compiler "r1592"]
                  [clj-v8 "0.1.3"]
                  [clj-v8-native "0.1.3"]
-                 [rhino/js "1.7R2"]]
+                 [org.mozilla/rhino "1.7R4"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]
                      [redd/native-deps "1.0.7"]]
   :jvm-opts ["-Djna.library.path=native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])

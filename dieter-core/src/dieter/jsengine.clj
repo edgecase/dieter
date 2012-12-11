4ce562a3cb45170423d95eae1780f7f871779302
Paul Biggar <paul.biggar@gmail.com>
2012-12-11 15:53:32 -0800

    Remove unused code.

--- dieter-core/src/dieter/jsengine.clj
+++ dieter-core/src/dieter/jsengine.clj
@@ -5,21 +5,6 @@
             [dieter.v8 :as v8]))
 
 
-(defmacro with-context [& body]
-  (if (= (:engine settings/*settings*) :rhino)
-    `(rhino/with-context ~@body)
-    `(v8/with-context ~@body)))
-
-(defmacro with-scope [pool preloads & body]
-  (if (= (:engine settings/*settings*) :rhino)
-    `(rhino/with-scope ~pool ~preloads ~@body)
-    `(v8/with-scope ~pool ~preloads ~@body)))
-
-(defn call [fn-name & args]
-  (if (= (:engine settings/*settings*) :rhino)
-    (apply rhino/call fn-name args)
-    (apply v8/call fn-name args)))
-
 ;; TODO: take an asset to avoid slurping here
 (defn run-compiler [pool preloads fn-name file & {:keys [engine]}]
   (let [input (slurp file)

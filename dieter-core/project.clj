2a8175f401bd62ab765842a0ac3c1b5f403c17f7
Paul Biggar <paul.biggar@gmail.com>
2012-12-16 14:42:30 -0800

    Update clj-v8 to 0.1.4, which should work on every platform with lein2

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -5,9 +5,7 @@
                  [fs "0.11.1"]
                  [clj-time "0.4.4"]
                  [com.google.javascript/closure-compiler "r1592"]
-                 [clj-v8 "0.1.3"]
-                 [clj-v8-native "0.1.3"]
+                 [clj-v8 "0.1.4"]
+                 [clj-v8-native "0.1.4"]
                  [org.mozilla/rhino "1.7R4"]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]
-                     [redd/native-deps "1.0.7"]]
-  :jvm-opts ["-Djna.library.path=native/macosx/x86_64:native/linux/x86_64:native/linux/x86:"])
+  :dev-dependencies [[org.clojure/clojure "1.3.0"]])

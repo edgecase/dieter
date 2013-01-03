409f7f00db5cbedc2885ce7aed8b23e51e143139
Paul Biggar <paul.biggar@gmail.com>
2013-01-03 01:11:13 -0800

    Remove tests for removed code

--- dieter-core/test/dieter/test/path.clj
+++ dieter-core/test/dieter/test/path.clj
@@ -1,6 +1,7 @@
 (ns dieter.test.path
   (:require [dieter.path :as path]
-            [dieter.cache :as cache])
+            [dieter.cache :as cache]
+            [dieter.settings :as settings])
   (:use clojure.test)
   (:require [clojure.java.io :as io]))
 
@@ -8,53 +9,27 @@
   (is (= "resources/asset-cache/assets/foo-d259b08a2dfaf8bf776cbadbe85442d3.js"
          (cache/cached-file-path "/assets/foo.js" "content string"))))
 
-(deftest test-find-file
-  (let [dir (io/file "test/fixtures/assets/javascripts/")]
+(deftest test-find-asset
+  (settings/with-options {:asset-root "test/fixtures"}
     (testing "relative path"
-      (let [file (path/find-file "./lib/framework.js" dir)]
+      (let [file (path/find-asset "./javascripts/lib/framework.js")]
         (is (re-matches #".*test/fixtures/assets/javascripts/(\./)?lib/framework.js$"
                         (.getPath file)))
         (is (.exists file)))
-      (is (nil? (path/find-file "./framework.js" dir))))
-
-    (testing "non-specific path"
-      (let [file (path/find-file "framework.js" dir)]
-        (is (re-matches #".*test/fixtures/assets/javascripts/lib/framework.js$" (.getPath file)))
-        (is (.exists file))))
-
-    (testing "partial path"
-      (let [file (path/find-file "lib/framework.js" dir)]
-        (is (re-matches #".*test/fixtures/assets/javascripts/lib/framework.js$" (.getPath file)))
-        (is (.exists file))))
-
-    (testing "no file extension"
-      (let [file (path/find-file "framework" dir)]
-        (is (re-matches #".*test/fixtures/assets/javascripts/lib/framework.js$" (.getPath file)))
-        (is (.exists file))))
-
-    (testing "different file extension"
-      (let [file (path/find-file "basic.css" (io/file "test/fixtures/assets/stylesheets/"))]
-        (is (re-matches #".*test/fixtures/assets/stylesheets/basic.less$" (.getPath file)))
-        (is (.exists file))))
+      (is (nil? (path/find-asset "./framework.js"))))
 
     (testing "no file exists"
-      (is (nil? (path/find-file "dontfindme.txt" dir))))))
+      (is (nil? (path/find-asset "dontfindme.txt")))))
+
+  (settings/with-options {:cache-root "test/fixtures"}
+   (testing "different file extension"
+     (let [file (path/find-asset "basic.css")]
+       (is (re-matches #".*test/fixtures/assets/stylesheets/basic.less$" (.getPath file)))
+       (is (.exists file))))))
 
 (defn file= [f1 f2]
   (= (.getCanonicalPath (io/file f1))
      (.getCanonicalPath (io/file f2))))
-
-(deftest test-search-dir
-  (is (file= "test/fixtures/assets/javascripts/lib/"
-             (path/search-dir "lib/framework.js" (io/file "test/fixtures/assets/javascripts/"))))
-  (is (file= "test/fixtures/assets/javascripts/"
-             (path/search-dir "./app.js" (io/file "test/fixtures/assets/javascripts/"))))
-  (is (file= "test/fixtures/assets/javascripts/lib/"
-             (path/search-dir "./lib/" (io/file "test/fixtures/assets/javascripts/"))))
-  (is (file= "test/fixtures/assets/javascripts/lib/"
-             (path/search-dir "./lib/" (.getParentFile (io/file "test/fixtures/assets/javascripts/manifest.js.dieter")))))
-  (is (file= "test/fixtures/assets/javascripts/models/"
-             (path/search-dir "./models/" (.getParentFile (io/file "test/fixtures/assets/javascripts/manifest.js.dieter"))))))
 
 (deftest test-add-md5
   (is (= "/assets/foo-acbd18db4cc2f85cedef654fccc4a4d8.js" (cache/add-md5 "/assets/foo.js" "foo")))

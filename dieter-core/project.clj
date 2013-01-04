f6ece509f898fa01f297346d7ca6d2fa03078e42
Paul Biggar <paul.biggar@gmail.com>
2013-01-04 12:00:18 -0800

    Don't depend on clojure 1.3, dev-depend on clojure 1.4

--- dieter-core/project.clj
+++ dieter-core/project.clj
@@ -4,12 +4,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "http://github.com/edgecase/dieter"
   :scm {:name "git" :dir ".."}
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [ring "1.0.1"]
+  :dependencies [[ring "1.0.1"]
                  [fs "0.11.1"]
                  [clj-time "0.4.4"]
                  [com.google.javascript/closure-compiler "r1592"]
                  [clj-v8 "0.1.4"]
                  [clj-v8-native "0.1.4"]
                  [org.mozilla/rhino "1.7R4"]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]])
+  :dev-dependencies [[org.clojure/clojure "1.4.0"]])

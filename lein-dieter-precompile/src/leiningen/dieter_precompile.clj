3bb061e5e3e7db77f8b3dd9fdfd98c5f1764525c
Dan Thiffault <dan@119labs.com>
2013-03-11 19:41:29 +0530

    Read/Write options to temp file to get around separate process com problems

--- lein-dieter-precompile/src/leiningen/dieter_precompile.clj
+++ lein-dieter-precompile/src/leiningen/dieter_precompile.clj
@@ -3,19 +3,23 @@
   (:require [clojure.string :as string]
             [leiningen.core.eval :as eval]))
 
-(defn eval-opt [project opt]
-  (eval/eval-in-project project `(var-get (resolve (symbol ~opt))) `(require (symbol (namespace (symbol ~opt))))))
+(defn eval-opt [project opt result-file]
+  (last [(eval/eval-in-project
+   project
+   `(with-open [w# (java.io.FileWriter. ~result-file)] (print-dup (var-get (resolve (symbol ~opt))) w#))
+   `(require (symbol (namespace (symbol ~opt))))) :from-file]))
 
-(defn resolve-dieter-options [project opt]
+(defn resolve-dieter-options [project opt result-file]
   (cond
    (map? opt) opt
-   (string? opt) (eval-opt project opt)))
+   (string? opt) (eval-opt project opt (.getAbsolutePath result-file))))
 
 (defn dieter-precompile
   [project]
-  (let [option-param (:dieter-options project)
-        options (resolve-dieter-options project option-param)]
-    (println (nil? options))
+  (let [result-file (java.io.File/createTempFile "lein-dieter" "options")
+        option-param (:dieter-options project)
+        options-result (resolve-dieter-options project option-param result-file)
+        options (if (= options-result :from-file) (read-string (slurp result-file)) options-result)]
     (println "options=" options)
     (eval/eval-in-project
      project

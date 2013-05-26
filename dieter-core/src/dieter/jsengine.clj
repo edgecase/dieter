6c2dbc4ec3df53357a2780ffd4a292fb61996c05
Paul Biggar <paul.biggar@gmail.com>
2013-05-26 15:00:42 -0700

    When there's an error in a js compiled file, add the filename to the stacktrace

--- dieter-core/src/dieter/jsengine.clj
+++ dieter-core/src/dieter/jsengine.clj
@@ -7,13 +7,22 @@
 
 ;; TODO: take an asset to avoid slurping here
 (defn run-compiler [pool preloads fn-name file & {:keys [engine]}]
-  (let [input (slurp file)
-        absolute (.getAbsolutePath file)
-        filename (.getCanonicalPath file)
-        args [input absolute filename]]
-    (if (or (= (:engine settings/*settings*) :rhino)
-            (= engine :rhino))
-      (rhino/with-scope pool preloads
-        (rhino/call fn-name args))
-      (v8/with-scope pool preloads
-        (v8/call fn-name args)))))
+  (try
+    (let [input (slurp file)
+          absolute (.getAbsolutePath file)
+          filename (.getCanonicalPath file)
+          args [input absolute filename]]
+      (if (or (= (:engine settings/*settings*) :rhino)
+              (= engine :rhino))
+        (rhino/with-scope pool preloads
+          (rhino/call fn-name args))
+        (v8/with-scope pool preloads
+          (v8/call fn-name args))))
+    (catch Exception e
+      (let [ste (StackTraceElement. "jsengine"
+                                    "compileHamlCoffee" (.getPath file) -1)
+            st (.getStackTrace e)
+            new-st (into [ste ] st)
+            new-st-array (into-array StackTraceElement new-st)]
+        (.setStackTrace e new-st-array)
+        (throw e)))))
